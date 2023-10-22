9e40b502386a48c0cf24e3cf4615fa97bc73f359
matiash26 <matias.henri26@gmail.com>
2023-10-22 19:44:34 -0300

    adicionando projeto novo e correção ortografica

--- js/main.js
+++ js/main.js
@@ -1,67 +1,66 @@
-const projectElement = document.querySelector(".projectContainer")
-const mobileMenu = document.querySelector(".mobileMenu")
-const headerColor = document.querySelector("header")
-const section = document.querySelectorAll(".hidden")
-const menu = document.querySelector(".menuList")
-const home = document.querySelector(".welcome")
-
+const projectElement = document.querySelector(".projectContainer");
+const mobileMenu = document.querySelector(".mobileMenu");
+const headerColor = document.querySelector("header");
+const section = document.querySelectorAll(".hidden");
+const menu = document.querySelector(".menuList");
+const home = document.querySelector(".welcome");
 
 function clickMenuMobile() {
-    menu.classList.toggle("openMenuMobile")
-    mobileMenu.children[1].classList.toggle("showExit")
+  menu.classList.toggle("openMenuMobile");
+  mobileMenu.children[1].classList.toggle("showExit");
 }
 function homeAction(entry) {
-    if (entry[0].isIntersecting) {
-        headerColor.classList.remove("navBlack")
-    } else {
-        headerColor.classList.add("navBlack")
-        
-    }
+  if (entry[0].isIntersecting) {
+    headerColor.classList.remove("navBlack");
+  } else {
+    headerColor.classList.add("navBlack");
+  }
 }
 function sectionAction(entries) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show")
-        } else {
-            entry.target.classList.remove("show")
-        }
-    })
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 }
 const listProject = [
-    {
-        title: "Dashboard - Free Lance",
-        image: "./assets/image/dashboard.jpg",
-        desc: "Projeto real para gerenciar as entradas e saidas dos produtos e ter melhoria nas vendas baseada no gráfico.",
-        tag: ["React JS", "Node JS express", "MySQL"],
-        repostory: "https://github.com/matiash26/Projeto-de-estoque-para-gerenciar-entradas-e-saidas",
-        site: ""
-    },
-    {
-        title: "CineMovies",
-        image: "./assets/image/site.png",
-        desc: "Projeto de site de filmes, na qual você precisa logar e fazer uma assinatura pra ter acesso (página toda fake) apenas para treinar.",
-        tag: ["Nextjs 13", "TypeScript"],
-        repostory: "https://github.com/matiash26/cinemovies",
-        site: "https://cinemovies-matiash26.vercel.app/login"
-    },
-    {
-        title: "Mercado não libre",
-        image: "./assets/image/mercadonaolivre.jpg",
-        desc: "repliquei a tela do mercado livre para testar meu conhecimento em Front-End",
-        tag: ["React JS"],
-        repostory: "https://github.com/matiash26/Mercado-nao-livre",
-        site: "https://mercado-nao-livre.vercel.app/",
-    },
+  {
+    title: "Community - Free Lance",
+    image: "./assets/image/community.png",
+    desc: "Projeto estilo a comunidade do twitter na qual será realizado postagem e usuários com permissão poderão validar se irá publicar ou não.",
+    tag: ["TypeScript", "Nextjs 13", "NextAuth", "Mysql", "Nodejs"],
+    repostory: "https://github.com/matiash26/Community",
+    site: "",
+  },
+  {
+    title: "Dashboard - Free Lance",
+    image: "./assets/image/dashboard.jpg",
+    desc: "Projeto real para gerenciar as entradas e saidas dos produtos e ter melhoria nas vendas baseada no gráfico.",
+    tag: ["React JS", "Node JS express", "MySQL"],
+    repostory:
+      "https://github.com/matiash26/Projeto-de-estoque-para-gerenciar-entradas-e-saidas",
+    site: "",
+  },
+  {
+    title: "Mercado não livre",
+    image: "./assets/image/mercadonaolivre.jpg",
+    desc: "repliquei a tela do mercado livre para testar meu conhecimento em Front-End",
+    tag: ["React JS"],
+    repostory: "https://github.com/matiash26/Mercado-nao-livre",
+    site: "https://mercado-nao-livre.vercel.app/",
+  },
+];
+const interHome = new IntersectionObserver(homeAction);
+interHome.observe(home);
 
-]
-const interHome = new IntersectionObserver(homeAction)
-interHome.observe(home)
-
-const interSection = new IntersectionObserver(sectionAction)
-section.forEach(el => interSection.observe(el))
-console.log(listProject.map(project => project.repostory))
-const projectCard = listProject.map(project => {
-    return (`
+const interSection = new IntersectionObserver(sectionAction);
+section.forEach((el) => interSection.observe(el));
+console.log(listProject.map((project) => project.repostory));
+const projectCard = listProject
+  .map((project) => {
+    return `
     <li class="ProjectContent">
     <h2>${project.title}</h2>
     <img src="${project.image}" alt="Projeto Dashboard">
@@ -71,16 +70,22 @@ const projectCard = listProject.map(project => {
             <p>${project.desc}</p>
         </div>
         <div class="tagsProject">
-            ${project.tag.map(tag => `<span>${tag}</span>`).join("")}
+            ${project.tag.map((tag) => `<span>${tag}</span>`).join("")}
         </div>
         <div class="buttonsProject">
-            <a class="btnProject" href="${project.repostory}" target="_blank">Repositório</a>
-           ${project.site && `<a class="btnProject" href='${project.site}' target="_blank">Acessar</a>`}
+            <a class="btnProject" href="${
+              project.repostory
+            }" target="_blank">Repositório</a>
+           ${
+             project.site &&
+             `<a class="btnProject" href='${project.site}' target="_blank">Acessar</a>`
+           }
         </div>
     </div>
-</li>`)
-}).join("")
+</li>`;
+  })
+  .join("");
 
-projectElement.innerHTML = projectCard
-mobileMenu.children[0].addEventListener("click", clickMenuMobile)
-mobileMenu.children[1].addEventListener("click", clickMenuMobile)
+projectElement.innerHTML = projectCard;
+mobileMenu.children[0].addEventListener("click", clickMenuMobile);
+mobileMenu.children[1].addEventListener("click", clickMenuMobile);
